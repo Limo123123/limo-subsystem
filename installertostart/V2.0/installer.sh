9f6c4dde6c072afb5a816e11d0e8473e3c26b04a
Limo123123 <90343619+Limo123123@users.noreply.github.com>
2023-11-19 14:31:23 +0100

    Update installer.sh

--- installertostart/V2.0/installer.sh
+++ installertostart/V2.0/installer.sh
@@ -7,8 +7,8 @@ limo=$(curl -O https://raw.githubusercontent.com/Limo123123/limo-subsystem/main/
 chmod +x $'limo'
 
 # Verschiebe die Dateien nach /usr/bin
-sudo mv $'updater' /usr/bin
-sudo mv $'limo' /usr/bin
+sudo mv updater /usr/bin
+sudo mv limo /usr/bin
 
 # Gebe eine Erfolgsmeldung aus
 if [ -f /usr/bin/updater ] && [ -f /usr/bin/limo ]; then
